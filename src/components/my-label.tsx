d9a5efaf998fc511fa7ad4aa78e867d05a42b781
LucianaCHA <lucianachamorro87@gmail.com>
2024-01-23 08:37:36 -0300

    story book ready

---
+++ src/components/my-label.tsx
@@ -0,0 +1,39 @@
+import './my-label.css'
+/**allCaps is a boolean that defaults to false
+ *  ! todas las propiedades son opcionales
+ * color is a string text-primary | text-secondary | text-tertiary
+ * fontColor con un selecto de color
+*/ 
+interface Props {
+  /**
+   * Label to display
+   */
+  label: string
+  /**
+   * Size of the label
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3'
+  /**
+   * True if the label should be all caps
+   */
+  allCaps?: boolean
+  /**
+   * Color of the label text
+   */
+  color?: 'text-primary' | 'text-secondary' | 'text-tertiary'
+  /**
+   * Choose the color of the label text!
+   */
+  fontColor?: string
+}
+export const MyLabel = ({
+  label,
+  size='normal',
+  allCaps=false,
+  color='text-primary',
+  fontColor=undefined
+}: Props) => {
+  return (
+    <span className ={`label ${size} ${allCaps ? 'all-caps' : ''} ${color}`} style={{color: fontColor}}>{label}</span>
+  )
+}
---
+++ src/stories/my-label.stories.ts
@@ -0,0 +1,57 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { MyLabel } from "../components/my-label";
+
+const meta ={ // meta es la info e la pgina que va a regir al edior visual
+    title :'Example/My label', // aca indico sonde quiero que se muestre
+    component: MyLabel,
+    tags:['autodocs'], // esto es para que se muestre en la pagina de autodocs
+    parameters:{
+        layout: 'centered'// dice que el componente se muestre centrado
+    },
+    argTypes:{
+        label:{
+            description: 'Texto del label',
+            defaultValue: 'Hello World',
+            control: 'text' // esto es para que se muestre en el editor visual
+        },
+        size:{
+            description: 'Tamaño del label',
+            defaultValue: 'medium',
+            control: {
+                type: 'inline-radio',
+                options: ['normal', 'h1', 'h2','h3']
+            }
+        },
+        }
+} satisfies Meta<typeof MyLabel> // esto es para que el editor visual sepa que es un componente de react y reconozca sus props
+
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+
+export const Default: Story = {
+    args: {
+        label: 'Hello World'
+    }
+}
+
+export const AllCaps: Story = {
+    args: {
+        label: 'Hello World',
+        allCaps: true
+    }
+}
+
+export const Secondary: Story = {
+    args: {
+        label: 'Hello World',
+        color: 'text-secondary'
+    }
+}
+
+export const CustomColor: Story = {
+    args: {
+        label: 'Hello World'
+    }
+}
